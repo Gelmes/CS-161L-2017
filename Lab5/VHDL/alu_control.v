5c2eef61458028faaf36acf410e64111b08e2eb8
BuildTools <marco.firsteye@gmail.com>
2017-05-11 15:59:46 -0700

    Fixed Memory init.coe bug

---
+++ Lab5/VHDL/alu_control.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+`include "constants.v"
+
+module alu_control(
+ input wire [1:0] alu_op ,
+ input wire [5:0] instruction_5_0 ,
+ output reg [3:0] alu_out
+);
+	 
+//Combinational Logic
+always @(*) begin
+	case(alu_op) 
+		2'b00: begin
+			alu_out = `ALU_ADD;
+			end
+		2'b01: begin
+			alu_out = `ALU_SUB;
+			end
+		2'b10, 2'b11: begin
+			case (instruction_5_0) 
+				`FUNCT_ADD: alu_out = `ALU_ADD;
+				`FUNCT_SUB: alu_out = `ALU_SUB;
+				`FUNCT_AND: alu_out = `ALU_AND;
+				`FUNCT_OR : alu_out = `ALU_OR;
+				`FUNCT_LT : alu_out = `ALU_LT;
+				`FUNCT_NOR : alu_out = `ALU_NOR;
+			endcase
+			end
+		default: begin
+			alu_out = `ALU_ADD;
+			end
+	endcase
+end
+
+
+endmodule
---
+++ Lab5/VHDL/isim/cs161_processor_testbench_isim_beh.exe.sim/work/m_00000000002975037628_1059438215.c
@@ -0,0 +1,206 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0xc3576ebc */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "C:/Users/Marco/Dropbox/School/SPRING 2017/161L/New Stuff/Lab4/VHDL/alu_control.v";
+static unsigned int ng1[] = {0U, 0U};
+static unsigned int ng2[] = {2U, 0U};
+static unsigned int ng3[] = {1U, 0U};
+static unsigned int ng4[] = {6U, 0U};
+static unsigned int ng5[] = {32U, 0U};
+static unsigned int ng6[] = {34U, 0U};
+static unsigned int ng7[] = {36U, 0U};
+static unsigned int ng8[] = {37U, 0U};
+static unsigned int ng9[] = {42U, 0U};
+static unsigned int ng10[] = {7U, 0U};
+static unsigned int ng11[] = {39U, 0U};
+static unsigned int ng12[] = {12U, 0U};
+static unsigned int ng13[] = {3U, 0U};
+
+
+
+static void Always_11_0(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    int t6;
+    char *t7;
+    char *t8;
+    int t9;
+
+LAB0:    t1 = (t0 + 2528U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(11, ng0);
+    t2 = (t0 + 2848);
+    *((int *)t2) = 1;
+    t3 = (t0 + 2560);
+    *((char **)t3) = t2;
+    *((char **)t1) = &&LAB4;
+
+LAB1:    return;
+LAB4:    xsi_set_current_line(11, ng0);
+
+LAB5:    xsi_set_current_line(12, ng0);
+    t4 = (t0 + 1048U);
+    t5 = *((char **)t4);
+
+LAB6:    t4 = ((char*)((ng1)));
+    t6 = xsi_vlog_unsigned_case_compare(t5, 2, t4, 2);
+    if (t6 == 1)
+        goto LAB7;
+
+LAB8:    t2 = ((char*)((ng3)));
+    t6 = xsi_vlog_unsigned_case_compare(t5, 2, t2, 2);
+    if (t6 == 1)
+        goto LAB9;
+
+LAB10:    t2 = ((char*)((ng2)));
+    t6 = xsi_vlog_unsigned_case_compare(t5, 2, t2, 2);
+    if (t6 == 1)
+        goto LAB11;
+
+LAB12:    t2 = ((char*)((ng13)));
+    t6 = xsi_vlog_unsigned_case_compare(t5, 2, t2, 2);
+    if (t6 == 1)
+        goto LAB13;
+
+LAB14:
+LAB16:
+LAB15:    xsi_set_current_line(29, ng0);
+
+LAB35:    xsi_set_current_line(30, ng0);
+    t3 = ((char*)((ng2)));
+    t7 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t7, t3, 0, 0, 4);
+
+LAB17:    goto LAB2;
+
+LAB7:    xsi_set_current_line(13, ng0);
+
+LAB18:    xsi_set_current_line(14, ng0);
+    t7 = ((char*)((ng2)));
+    t8 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t8, t7, 0, 0, 4);
+    goto LAB17;
+
+LAB9:    xsi_set_current_line(16, ng0);
+
+LAB19:    xsi_set_current_line(17, ng0);
+    t3 = ((char*)((ng4)));
+    t4 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t4, t3, 0, 0, 4);
+    goto LAB17;
+
+LAB11:    xsi_set_current_line(19, ng0);
+
+LAB20:    xsi_set_current_line(20, ng0);
+    t3 = (t0 + 1208U);
+    t4 = *((char **)t3);
+
+LAB21:    t3 = ((char*)((ng5)));
+    t9 = xsi_vlog_unsigned_case_compare(t4, 6, t3, 6);
+    if (t9 == 1)
+        goto LAB22;
+
+LAB23:    t2 = ((char*)((ng6)));
+    t6 = xsi_vlog_unsigned_case_compare(t4, 6, t2, 6);
+    if (t6 == 1)
+        goto LAB24;
+
+LAB25:    t2 = ((char*)((ng7)));
+    t6 = xsi_vlog_unsigned_case_compare(t4, 6, t2, 6);
+    if (t6 == 1)
+        goto LAB26;
+
+LAB27:    t2 = ((char*)((ng8)));
+    t6 = xsi_vlog_unsigned_case_compare(t4, 6, t2, 6);
+    if (t6 == 1)
+        goto LAB28;
+
+LAB29:    t2 = ((char*)((ng9)));
+    t6 = xsi_vlog_unsigned_case_compare(t4, 6, t2, 6);
+    if (t6 == 1)
+        goto LAB30;
+
+LAB31:    t2 = ((char*)((ng11)));
+    t6 = xsi_vlog_unsigned_case_compare(t4, 6, t2, 6);
+    if (t6 == 1)
+        goto LAB32;
+
+LAB33:
+LAB34:    goto LAB17;
+
+LAB13:    goto LAB11;
+
+LAB22:    xsi_set_current_line(21, ng0);
+    t7 = ((char*)((ng2)));
+    t8 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t8, t7, 0, 0, 4);
+    goto LAB34;
+
+LAB24:    xsi_set_current_line(22, ng0);
+    t3 = ((char*)((ng4)));
+    t7 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t7, t3, 0, 0, 4);
+    goto LAB34;
+
+LAB26:    xsi_set_current_line(23, ng0);
+    t3 = ((char*)((ng1)));
+    t7 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t7, t3, 0, 0, 4);
+    goto LAB34;
+
+LAB28:    xsi_set_current_line(24, ng0);
+    t3 = ((char*)((ng3)));
+    t7 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t7, t3, 0, 0, 4);
+    goto LAB34;
+
+LAB30:    xsi_set_current_line(25, ng0);
+    t3 = ((char*)((ng10)));
+    t7 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t7, t3, 0, 0, 4);
+    goto LAB34;
+
+LAB32:    xsi_set_current_line(26, ng0);
+    t3 = ((char*)((ng12)));
+    t7 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t7, t3, 0, 0, 4);
+    goto LAB34;
+
+}
+
+
+extern void work_m_00000000002975037628_1059438215_init()
+{
+	static char *pe[] = {(void *)Always_11_0};
+	xsi_register_didat("work_m_00000000002975037628_1059438215", "isim/cs161_processor_testbench_isim_beh.exe.sim/work/m_00000000002975037628_1059438215.didat");
+	xsi_register_executes(pe);
+}
